a3bc710d7b396f7e82e0a8877fe1f57702c3c319
Chindalath Traymany <chindalatht@gmail.com>
2020-07-09 04:29:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     nil
   else
-    min_key = ""
+    min_key
+    min_value
     name_hash.each do |name, value|
-      min_key 
+      value < min_value
     end
   end
   min_key
